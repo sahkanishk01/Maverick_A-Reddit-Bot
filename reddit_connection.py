a962f671a233c3eea3acfb9c94e6f7aa73261b8c
Kanishk Sah <128390480+sahkanishk01@users.noreply.github.com>
2025-07-11 15:09:14 +0530

    Update reddit_connection.py

--- reddit_connection.py
+++ reddit_connection.py
@@ -6,12 +6,12 @@ reddit = None
 def init_reddit_connection():
     global reddit
     reddit = praw.Reddit(
-            client_id=config.CLIENT_ID,
-            client_secret=config.CLIENT_SECRET,
-            refresh_token=config.ACCESS_TOKEN,
-            user_agent=config.USERAGENT,
-            user_name=config.USERNAME,
-            password=config.PASSWORD
+            client_id = config.CLIENT_ID,
+            client_secret = config.CLIENT_SECRET,
+            refresh_token = config.ACCESS_TOKEN,
+            user_agent = config.USERAGENT,
+            user_name = config.USERNAME,
+            password = config.PASSWORD
         )
 
 
